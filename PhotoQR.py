17dfd3fc90ee30737e83b50f4ed413c78be24b39
NejcGranda <nejc.granda10136@gmail.com>
2023-12-18 14:18:43 +0100

    spremembe

--- PhotoQR.py
+++ PhotoQR.py
@@ -1,8 +1,9 @@
 import random
 import string
 import qrcode
+from os.path import exists
 
-def koda(dolzina):
+def generiraj_kodo(dolzina):
     koda = ""
 
     
@@ -27,16 +28,60 @@ def QRkoda(koda):
         version = 1,
         error_correction = qrcode.constants.ERROR_CORRECT_H,
         box_size = 10,
-        border = 1
+        border = 0
     )
-    url = "https://svstefan.si/slike/"
+    url = "https://svstefan.si/slike/" + koda + "/"
     qr.add_data(url)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save("sv stefan_qr.png")
+    img.save("kode/" + koda + ".png")
 
     return img
 
+def ali_uporabljena(koda):
+    
+    try:
+        file = open("kode_vse.txt", "r")
+    except:
+        return False
 
-koda = koda(5)
+    while True:
+        line = file.readline()
 
-QRkoda(koda)
+        if line == koda:
+            file.close()
+            return True
+
+        if not line:
+            file.close()
+            return False
+
+def shrani_kodo(koda, datoteka):
+    file = open(datoteka, "a")
+    file.writelines(koda + "\n")
+    file.close
+
+def ustvari_pdf(kode):
+    
+    return True
+
+stevilo = 10
+dolzina = 8
+
+kode = []
+
+for i in range(stevilo):
+    koda = generiraj_kodo(dolzina)
+
+    while ali_uporabljena(koda) == True:
+        koda = generiraj_kodo(dolzina)
+
+    kode.append(koda)
+    shrani_kodo(koda, "kode_vse.txt")
+    shrani_kodo(koda, "kode_na_voljo.txt")
+    QRkoda(koda)
+    print(str(i) + ": " + koda)
+
+
+
+
+# ime: datum, ura
---
+++ UporabiKodo.py
@@ -0,0 +1,50 @@
+# Vzameš 1 kodo iz prve vrstice datoteke kode_na_voljo.txt in jo izbrišeš
+# Če kode niso na voljo, napiši napako, da kode manjkajo
+# Vzameš vse datoteke iz "src" in je premkaneš v "dst" z novim imenom: "<koda>_<original_ime_datoteke>"
+
+# pypdf
+import os
+import shutil
+
+
+SRC_FOLDER = "./src"
+DST_FOLDER = "./dst"
+
+
+def izberi_kodo(datoteka):
+    with open(datoteka, "r") as fin:
+        data = fin.read().splitlines(True)
+
+    if len(data) == 0:
+        return False
+    
+    with open(datoteka, "w") as fout:
+        fout.writelines(data[1:])
+
+    return data[0].strip()
+
+
+def premakni_file(koda, src_folder, dst_folder):
+
+    for file_name in os.listdir(src_folder):
+        src_file = src_folder + "/" + file_name
+        dst_file = dst_folder + "/" + koda + "_" + file_name
+        if os.path.isfile(src_file):
+            shutil.move(src_file, dst_file)
+    
+#### MAIN ###
+
+# Preveri če obstajajo datoteke v mapi src !
+
+izbrana = izberi_kodo("kode_na_voljo.txt")
+
+if izbrana == False:
+    print("Ni več prostih kod")
+    exit(0)
+
+# kode_uporabljene.txt
+
+print("Izbrana koda: " + izbrana)
+
+premakni_file(izbrana, SRC_FOLDER, DST_FOLDER)
+
