d8111fbcd5a1620c78a604198469fc28d81296d3
NejcGranda <nejc.granda10136@gmail.com>
2023-12-20 22:30:50 +0100

    QR kode na pravem mestu

--- PhotoQR.py
+++ PhotoQR.py
@@ -31,21 +31,19 @@ def QRkoda(koda):
         version = 1,
         error_correction = qrcode.constants.ERROR_CORRECT_H,
         box_size = 10,
-        border = 0
+        border = 0,
     )
 
     url = "https://svstefan.si/slike/" + koda + "/"
     qr.add_data(url)
     qr.make(fit = True)
+
     img = qr.make_image(fill_color="black", back_color="white")
-
-    mm_to_px = 4
-
-    img = img.resize((30 * mm_to_px, 30 * mm_to_px))
 
     img.save("kode/" + koda + ".png")
 
-    return img
+
+    return True
 
 
 
@@ -73,42 +71,68 @@ def shrani_kodo(koda, datoteka):
     file.close
 
 def ustvari_pdf(koda):
-    pdf = fitz.open()
-    page = pdf.new_page()
-    print(page.rect)
+    pdf = fitz.open("SvStefan-Photo-ThankYou-Card-Print.pdf")
+    page = pdf[0]
     img_path = "kode/" + koda + ".png"
 
     mm_to_points = 2.83456
-    px_to_points = 0.75
-    mm_width, mm_height = 30, 30
-    width_in_points = mm_width * mm_to_points
-    height_in_points = mm_height * mm_to_points
+#QR
+    QR_size_mm = 30
+    top_margin_QR_mm = 30.126
+    left_margin_QR_mm = 3.682
+    left_margin_QR_mm1 = 108.682 
 
+    QR_size = QR_size_mm * mm_to_points
+    top_margin_QR = top_margin_QR_mm * mm_to_points
+    left_margin_QR = left_margin_QR_mm * mm_to_points
+    left_margin_QR1 = left_margin_QR_mm1 * mm_to_points
 
-    levi_rob = 3.682 * mm_to_points
-    levi_rob1 = 108.682
-    zgornji_rob = 30.126 * mm_to_points
+#TEXT
+    text_size_mm_x = 24.346
+    text_size_mm_y = 7.255
+    top_margin_text_mm = 54.539
+    left_margin_text_mm = 36.46
+    left_margin_text_mm1 = 141.46
+
+    text_size_x = text_size_mm_x * mm_to_points
+    text_size_y = text_size_mm_y * mm_to_points
+    top_margin_text = top_margin_text_mm * mm_to_points
+    left_margin_text = left_margin_text_mm * mm_to_points
+    left_margin_text1 = left_margin_text_mm1 * mm_to_points
+
     
+    font_name = "hebo"
+
 
     for i in range(5):
     
+    #QR
         img = fitz.open(img_path)
-        rect = fitz.Rect(levi_rob, zgornji_rob, levi_rob + (90 * mm_to_points), zgornji_rob + (90 * mm_to_points))
-        page.insert_image(rect, filename = img_path)
+        rectQR_L = fitz.Rect(left_margin_QR, top_margin_QR, left_margin_QR + QR_size, top_margin_QR + QR_size)
+        page.insert_image(rectQR_L, filename = img_path)
 
-        zgornji_rob += 50 * mm_to_points
+        rectQR_L = fitz.Rect(left_margin_QR1, top_margin_QR, left_margin_QR1 + QR_size, top_margin_QR + QR_size)
+        page.insert_image(rectQR_L, filename = img_path)
+
+    #TEXT
+        
+        text = koda
+        textbox_rect = fitz.Point(left_margin_text, top_margin_text, left_margin_text + text_size_x, top_margin_text + text_size_y)
+        textbox = page.insertTextbox(textbox_rect, text, fontsize = 10 )
+
+
+        top_margin_QR += (57 * mm_to_points)
+        top_margin_text += (57 * mm_to_points)
 
 
     pdf.save("sv-stefan.pdf")
     pdf.close()
 
-    return True
-
 
 #### MAIN ####
 
 
-stevilo = 1
+stevilo = 5
 dolzina = 8
 
 kode = []
